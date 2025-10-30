5acdfb3a896dd58c7b7c071ae468111a5c31b049
rain9 <15911122312@163.com>
2025-10-30 14:39:09 +0800

    chore: add css plugin

--- packages/EntityUI/vite.config.ts
+++ packages/EntityUI/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), dts({ rollupTypes: true }), cssInjectedByJsPlugin()],
   build: {
     lib: {
       entry: "src/components/index.tsx",
