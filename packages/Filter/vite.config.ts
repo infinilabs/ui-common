4dc9743d881e945402213dfbcf94badd80702475
ayang <473033518@qq.com>
2025-12-26 11:53:23 +0800

    chore: support Packaging

--- packages/Filter/vite.config.ts
+++ packages/Filter/vite.config.ts
@@ -1,15 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import UnoCSS from "unocss/vite";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react(),
     UnoCSS(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: "./tsconfig.app.json",
+    }),
   ],
+  build: {
+    lib: {
+      entry: "src/components/index.tsx",
+      name: "Filter",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "antd",
+        "lucide-react",
+        "clsx",
+        "tailwind-merge",
+      ],
+    },
+  },
 });
