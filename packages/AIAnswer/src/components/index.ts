1b5d1e49f77ebdf4799533bde61f9f5e9c38ef90
rain9 <15911122312@163.com>
2026-01-06 16:31:57 +0800

    chore: theme & icon color

---
+++ packages/AIAnswer/src/components/index.ts
@@ -0,0 +1,7 @@
+import "./index.css";
+
+export { AIAnswer } from "./AIAnswer";
+export { AIAnswerActions } from "./AIAnswerActions";
+export { AIContinueButton } from "./AIContinueButton";
+export { AIExpandToggle } from "./AIExpandToggle";
+export type { AIAnswerProps } from "./AIAnswer";
--- packages/AIAnswer/src/index.ts
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-
-export { AIAnswer } from "./components/AIAnswer";
-export { AIAnswerActions } from "./components/AIAnswerActions";
-export { AIContinueButton } from "./components/AIContinueButton";
-export { AIExpandToggle } from "./components/AIExpandToggle";
-export type { AIAnswerProps } from "./components/AIAnswer";
