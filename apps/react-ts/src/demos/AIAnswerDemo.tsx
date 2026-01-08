03ea424de6f613da63175e9a13495d04ad48c9d8
rain9 <15911122312@163.com>
2026-01-08 11:23:07 +0800

    chore: update dark

--- apps/react-ts/src/demos/AIAnswerDemo.tsx
+++ apps/react-ts/src/demos/AIAnswerDemo.tsx
@@ -82,7 +82,6 @@ export default function AIAnswerDemo() {
         expandText="展开更多"
         collapseText="收起"
         continueLabel="继续追问"
-        theme={theme}
         onContinue={() => window.alert("继续追问")}
       />
       </div>
