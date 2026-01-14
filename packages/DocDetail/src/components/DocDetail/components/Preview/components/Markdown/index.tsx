eff8a02099a421c464ee0b464ae1b4a176556767
ayang <473033518@qq.com>
2026-01-14 17:28:33 +0800

    feat(doc-detail): support code highlighting in markdown

--- packages/DocDetail/src/components/DocDetail/components/Preview/components/Markdown/index.tsx
+++ packages/DocDetail/src/components/DocDetail/components/Preview/components/Markdown/index.tsx
@@ -4,8 +4,9 @@ import {
   type XMarkdownProps,
 } from "@ant-design/x-markdown";
 import { useEffect, useState, type FC } from "react";
-import { Mermaid } from "@ant-design/x";
+import { Mermaid, CodeHighlighter } from "@ant-design/x";
 import { Typography } from "antd";
+import "./index.css";
 
 const { Text } = Typography;
 
@@ -13,10 +14,16 @@ const Code: FC<ComponentProps> = (props) => {
   const { className, children } = props;
   const lang = className?.match(/language-(\w+)/)?.[1] ?? "";
 
+  console.log("lang", lang);
+
   if (typeof children !== "string") return null;
 
   if (lang === "mermaid") {
     return <Mermaid>{children}</Mermaid>;
+  }
+
+  if (lang) {
+    return <CodeHighlighter lang={lang}>{children}</CodeHighlighter>;
   }
 
   return <Text code>{children}</Text>;
