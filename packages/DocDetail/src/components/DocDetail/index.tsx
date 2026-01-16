87932af1e6448ac3e9ff642da04d57af9cee4cbd
ayang <473033518@qq.com>
2026-01-16 10:10:06 +0800

    refactor(doc-detail): replace dropdown icon

--- packages/DocDetail/src/components/DocDetail/index.tsx
+++ packages/DocDetail/src/components/DocDetail/index.tsx
@@ -1,14 +1,8 @@
 import { Typography } from "antd";
-import { ChevronRight, Dot, Ellipsis, Minus } from "lucide-react";
+import { ChevronDown, ChevronRight, Dot, Minus } from "lucide-react";
 import { motion } from "motion/react";
 
-import {
-  Fragment,
-  useState,
-  type FC,
-  type HTMLAttributes,
-  type ReactNode,
-} from "react";
+import { useState, type FC, type HTMLAttributes, type ReactNode } from "react";
 import Preview from "./components/Preview";
 import AIInterpretation from "./components/AIInterpretation";
 import { cn } from "@/utils/cn";
@@ -58,7 +52,7 @@ export interface DocDetailProps extends HTMLAttributes<HTMLDivElement> {
     tags?: string[];
     url?: string;
     size?: ReactNode;
-    owner: {
+    owner?: {
       type?: string;
       id?: string;
       icon?: string;
@@ -84,7 +78,7 @@ export interface DocDetailProps extends HTMLAttributes<HTMLDivElement> {
 const DocDetail: FC<DocDetailProps> = (props) => {
   const { data, i18n, actionButtons, className, ...rest } = props;
 
-  const [visibleMore, setVisibleMore] = useState(false);
+  const [expandMore, setExpandMore] = useState(false);
 
   const moreInfo = [
     {
@@ -133,28 +127,29 @@ const DocDetail: FC<DocDetailProps> = (props) => {
           <Dot className="size-3" />
           <div>{data?.updated ?? "-"}</div>
 
-          <Ellipsis
-            className="ml-2 size-3 hover:text-primary transition cursor-pointer"
+          <ChevronDown
+            className={cn(
+              "ml-2 size-3 hover:text-primary transition cursor-pointer",
+              {
+                "-scale-y-100": expandMore,
+              }
+            )}
             onClick={() => {
-              setVisibleMore((prev) => !prev);
+              setExpandMore((prev) => !prev);
             }}
           />
         </Text>
 
-        <div className="inline-flex gap-2">
-          {actionButtons?.map((button, index) => (
-            <Fragment key={index}>{button}</Fragment>
-          ))}
-        </div>
+        <div className="inline-flex gap-2">{actionButtons}</div>
       </div>
 
       <motion.div
         className="bg-black/3 dark:bg-white/4 rounded-lg overflow-hidden"
         initial={false}
         animate={{
-          height: visibleMore ? "auto" : 0,
-          opacity: visibleMore ? 1 : 0,
-          marginBottom: visibleMore ? "1rem" : 0,
+          height: expandMore ? "auto" : 0,
+          opacity: expandMore ? 1 : 0,
+          marginBottom: expandMore ? "1rem" : 0,
         }}
       >
         <div className="flex flex-wrap gap-row-2 p-4">
