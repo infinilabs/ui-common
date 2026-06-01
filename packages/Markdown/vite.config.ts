7d0e6ece277416586335725a50a311825018489c
yaojiping <yaojiping@infini.ltd>
2026-06-01 16:29:35 +0800

    chore: adjust build of markdown

--- packages/Markdown/vite.config.ts
+++ packages/Markdown/vite.config.ts
@@ -14,7 +14,31 @@ export default defineConfig({
       rollupTypes: true,
       insertTypesEntry: true,
     }),
-    cssInjectedByJs(),
+    cssInjectedByJs({
+      injectCodeFunction: function (cssCode, options) {
+        try {
+          const doc = (globalThis as { document?: any }).document;
+          if (doc) {
+            var style = doc.createElement('style');
+
+            for (var attribute in options.attributes) {
+              style.setAttribute(attribute, options.attributes[attribute]);
+            }
+
+            style.appendChild(doc.createTextNode(cssCode));
+            var host = doc.querySelector('[data-fullscreen-host]');
+            var shadowRoot = host && host.shadowRoot;
+
+            if (shadowRoot) {
+              shadowRoot.appendChild(style);
+            } else {
+              doc.head.appendChild(style);
+            }
+          }
+        } catch {
+        }
+      }
+    }),
   ],
   build: {
     lib: {
